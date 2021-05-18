43d204a284bf1fec69965060cab1d13a25e13bcb
Austin Fisher <Mr.ATFisher@gmail.com>
2021-05-18 16:42:44 -0700

    finished baseline ML model

--- dataframe.py
+++ dataframe.py
@@ -1,12 +1,16 @@
 import os
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import cv2
-import keras
+from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers
 from keras.models import Sequential
 from keras.optimizers import RMSprop
 from keras.utils.np_utils import to_categorical
+from keras.callbacks import ReduceLROnPlateau
+
+from matplotlib import pyplot as plt
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -16,7 +20,7 @@ IMAGE_SIZE = 28
 target_directories = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   
 cwd = os.getcwd()
-df = pd.DataFrame(columns=['image_data', 'label'])
+data = []
 save_path = f'{cwd}\\processed_images\\'
 
 if not os.path.isdir(save_path):
@@ -47,19 +51,28 @@ for dir in target_directories:
         color = [255, 255, 255]
         img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
 
-        data = np.matrix(img)
-        df.loc[filename] = [data, dir]
+        data.append([img, dir])
 
-X = df['image_data']
-X_train = X/255.0
-X_train = X_train.values.reshape(-1,28,28,1)
-y = df['label']
-Y_train = to_categorical(y, num_classes = 10)
+X_train = []
+Y_train = []
+
+for feature, label in data:
+  X_train.append(feature)
+  Y_train.append(label)
+
+# Normalize the data
+X_train = np.array(X_train) / 255
+
+X_train = X_train.reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 1)
+Y_train = to_categorical(Y_train, num_classes = 10)
+
+# Split the train and the validation set for the fitting
+# X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=53)
 
 model = Sequential()
 
 model.add(layers.Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
-                 activation ='relu', input_shape = (28,28)))
+                 activation ='relu', input_shape = (28,28,1)))
 model.add(layers.Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
                  activation ='relu'))
 model.add(layers.MaxPool2D(pool_size=(2,2)))
@@ -79,19 +92,18 @@ model.add(layers.Dense(256, activation = "relu"))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(10, activation = "softmax"))
 
-# model.add(layers.Conv2D(3, (3,3), strides=1, input_shape=(28,28)))
-# model.add(layers.MaxPool2D(pool_size=(2,2), strides=1))
-# model.add(layers.Conv2D(2, (2,2), activation='relu'))
-# model.add(layers.Flatten())
-# model.add(layers.Dense(256, activation = "relu"))
-# model.add(layers.Dropout(0.5))
-# model.add(layers.Dense(10, activation = "softmax"))
-
 optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
 model.compile(optimizer=optimizer , loss="categorical_crossentropy", metrics=["accuracy"])
 
-epochs = 8 
-batch_size = 32
+# Set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
 
-model.fit(x=X_train, y=Y_train, batch_size=batch_size, epochs=epochs)
+epochs = 30 
+batch_size = 8
+
+history = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs)
